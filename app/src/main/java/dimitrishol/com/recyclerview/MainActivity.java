74cb48c2da8204bf51682c73eaf27c0bd2042211
Dimitris Hol <dimitrishol@protonmail.com>
2019-09-08 01:23:44 +0300

    Changed from static display of data, to fetching news with GET request.

--- app/src/main/java/dimitrishol/com/recyclerview/MainActivity.java
+++ app/src/main/java/dimitrishol/com/recyclerview/MainActivity.java
@@ -5,6 +5,20 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -13,65 +27,131 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
 
+    private ArrayList<String> imageUrls = new ArrayList<String>();
     private ArrayList<String> titles = new ArrayList<String>();
     private ArrayList<String> descriptions = new ArrayList<String>();
+    private ArrayList<String> dates = new ArrayList<String>();
+    private ArrayList<String> sources = new ArrayList<String>();
+    private ArrayList<String> urls= new ArrayList<String>();
+
+    private RequestQueue mQueue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        initiateNews();
-        initiateRecyclerView();
-        
+
+//        initiateNews();
+//        initiateRecyclerView();
+
+
+        // Get refference to the button
+        Button fetchButton = findViewById(R.id.fetch_data_button);
+
+        // Create Volley Queue
+
+
+        fetchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(TAG, "onClick: Pressed the Parse Button");
+                jsonParse();
+
+            }
+        });
+
+
+
     }
 
-    private void initiateNews(){
-
+    private void initiateNews() {
 
 
         titles.add("Corgi does cute stuff");
         descriptions.add("Corgi was really cute when it did stuff");
-
         titles.add("Husky does cute stuff");
         descriptions.add("Husky was really cute when it did stuff");
         titles.add("Husky does cute stuff");
         descriptions.add("Husky was really cute when it did stuff");
-        titles.add("Husky does cute stuff");
-        descriptions.add("Husky was really cute when it did stuff");
-        titles.add("Husky does cute stuff");
-        descriptions.add("Husky was really cute when it did stuff");
-        titles.add("Husky does cute stuff");
-        descriptions.add("Husky was really cute when it did stuff");
-        titles.add("Husky does cute stuff");
-        descriptions.add("Husky was really cute when it did stuff");
-        titles.add("Husky does cute stuff");
-        descriptions.add("Husky was really cute when it did stuff");
-        titles.add("Husky does cute stuff");
-        descriptions.add("Husky was really cute when it did stuff");
-
         titles.add("Golder Retriever does cute stuff");
         descriptions.add("Golden Retriever was really cute when it did stuff");
-
         titles.add("Dog does cute stuff");
         descriptions.add("Dog was really cute when it did stuff");
 
         Log.d(TAG, "RV : Got the news");
     }
-    
-    
-    private void initiateRecyclerView(){
 
 
+    private void jsonParse() {
+
+
+        // Initiate the Request Queue
+        String url = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-08-07&sortBy=publishedAt&apiKey=1b2ce28eaee94688ba98a1854d139e1c";
+        mQueue = Volley.newRequestQueue(this);
+
+
+        // Request a string rensponse from the URL
+        JsonRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+//                        Log.d(TAG, "onResponse: " + response);
+
+                        try {
+
+                            JSONArray articles = response.getJSONArray("articles");
+
+                            for (int i = 0; i < 10; i++) {
+
+                                JSONObject article = articles.getJSONObject(i);
+
+                                JSONObject source = article.getJSONObject("source");
+                                sources.add(source.getString("name"));
+
+                                imageUrls.add(article.getString("urlToImage"));
+                                titles.add(article.getString("title"));
+                                descriptions.add(article.getString("description"));
+                                dates.add(article.getString("publishedAt"));
+
+                                urls.add(article.getString("url"));
+
+
+                            }
+
+
+                            initiateRecyclerView();
+
+                        } catch(JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        error.printStackTrace();
+                    }
+                });
+
+
+        mQueue.add(jsonRequest);
+    }
+
+
+    private void initiateRecyclerView() {
 
 
         // Get the refference to the widget.
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewList);
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewList);
 
         Log.d(TAG, "RV : Got the reffecnce to RV.");
         // Context, and the 2 arrays. Context is the current activity.
-        NewsAdapter adapter = new NewsAdapter(MainActivity.this , titles, descriptions);
+        NewsAdapter adapter = new NewsAdapter(MainActivity.this , imageUrls , titles, descriptions, dates, sources , urls);
+
 
         Log.d(TAG, "RV : Created the news Adapter");
         // Finally set the adapter to the RV.
@@ -79,8 +159,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
-    
-    
-    
-    
+
+
 }
--- app/src/main/java/dimitrishol/com/recyclerview/NewsAdapter.java
+++ app/src/main/java/dimitrishol/com/recyclerview/NewsAdapter.java
@@ -7,7 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -16,13 +19,25 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private static final String TAG = "NewsAdapter";
 
+    private ArrayList<String> imageUrls = new ArrayList<String>();
     private ArrayList<String> titles = new ArrayList<String>();
     private ArrayList<String> descriptions = new ArrayList<String>();
+    private ArrayList<String> dates = new ArrayList<String>();
+    private ArrayList<String> sources = new ArrayList<String>();
+    private ArrayList<String> urls= new ArrayList<String>();
+
+    // This might be not actually useful ?
     private Context context;
 
-    public NewsAdapter(Context context , ArrayList<String> titles, ArrayList<String> descriptions) {
+    public NewsAdapter(Context contextArrayList , ArrayList<String> imageUrls, ArrayList<String> titles, ArrayList<String> descriptions, ArrayList<String> dates, ArrayList<String> sources, ArrayList<String> urls) {
+
+        this.imageUrls = imageUrls;
         this.titles = titles;
         this.descriptions = descriptions;
+        this.dates = dates;
+        this.sources = sources;
+        this.urls = urls;
+
         this.context = context;
     }
 
@@ -48,8 +63,13 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         Log.d(TAG, "onBindViewHolder: called." + " " + position);
 
         // The View holder, that has the 2 TextViews, gets the text from the array in the class.
+
+        Picasso.get().load(imageUrls.get(position)).into(viewHolder.image);
+
         viewHolder.title.setText(titles.get(position));
         viewHolder.description.setText(descriptions.get(position));
+        viewHolder.author.setText(sources.get(position));
+        viewHolder.date.setText(dates.get(position));
 
         // This is where you might also create a onClickListener
     }
@@ -63,14 +83,23 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
 
+        ImageView image;
         TextView title;
         TextView description;
+        TextView author;
+        TextView date;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            title = itemView.findViewById(R.id.title);
-            description = itemView.findViewById(R.id.description);
+            image = itemView.findViewById(R.id.articleImage);
+            title = itemView.findViewById(R.id.articleTitle);
+            description = itemView.findViewById(R.id.articleDesc);
+            author = itemView.findViewById(R.id.articleAuthor);
+            date = itemView.findViewById(R.id.articleDate);
+
+
 
         }
     }
